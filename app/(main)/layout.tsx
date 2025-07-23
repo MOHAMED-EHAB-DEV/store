74fb12cd8285bd05b813eb8391978efa9eab906f
Mohammed Ehab <mohamedehabam99@hotmail.com>
2025-07-23 14:33:57 +0300

    .

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Footer from "@/components/home/Footer";
+import Navbar from "@/components/home/Navbar";
+
+export default function RootLayout({
+                               children,
+                           }: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className="relative z-10 flex flex-col min-h-screen">
+            <Navbar/>
+
+            <div id="main-content" className="flex-1 flex flex-col items-center gap-3 justify-start">
+                {children}
+            </div>
+
+            <Footer/>
+        </div>
+    )
+}

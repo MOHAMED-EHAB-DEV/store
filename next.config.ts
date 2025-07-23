1a5e32d57c43ce89c6536c440691a0c1566f51f3
Mohammed Ehab <mohamedehabam99@hotmail.com>
2025-07-23 14:37:37 +0300

    .

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,7 @@ silent: !process.env.CI,
 widenClientFileUpload: true,
 
 // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-// This can increase your server load as well as your hosting bill.
+// This can increase your (auth) load as well as your hosting bill.
 // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
 // side errors will fail.
 // tunnelRoute: "/monitoring",

3a68e222353ddd95a4169d8a5f734e3a5f6786b2
Mohammed Ehab <mohamedehabam99@hotmail.com>
2025-07-26 21:39:50 +0300

    .

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-      optimizePackageImports: ["gsap", "lucide-react", "motion"],
-  },
-  ignoreBuildErrors: true
+    experimental: {
+        optimizePackageImports: ["gsap", "lucide-react", "motion"],
+    },
+    typescript: {
+        ignoreBuildErrors: true
+    }
 };
 
 export default nextConfig;

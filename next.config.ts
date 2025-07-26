103f1d74f45ba3645ce2d20e2582268535632727
Mohammed Ehab <mohamedehabam99@hotmail.com>
2025-07-26 21:23:37 +0300

    .

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
       optimizePackageImports: ["gsap", "lucide-react", "motion"],
-  }
+  },
+  ignoreBuildErrors: true
 };
 
 export default nextConfig;

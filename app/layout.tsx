b08e664df44070226d176c9e472b2a204719762a
Mohammed Ehab <mohamedehabam99@hotmail.com>
2025-08-03 11:31:22 +0300

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,8 @@ import { Toaster } from "@/components/ui/sonner"
 
 gsap.registerPlugin(useGSAP);
 
+export const dynamic = "force-dynamic";
+
 export const viewport = {
     width: 'device-width',
     initialScale: 1,

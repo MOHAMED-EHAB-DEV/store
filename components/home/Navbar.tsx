e01b8ca1a545ff39498a7181761b162093268898
Mohammed Ehab <mohamedehabam99@hotmail.com>
2025-08-05 14:32:43 +0300

    .

--- components/home/Navbar.tsx
+++ components/home/Navbar.tsx
@@ -131,14 +131,14 @@ const MobileDrawer = () => {
                                     </li>
                                 ))}
                                 <Link
-                                    className="outline-none cursor-pointer hover:scale-105 transition-all duration-500 border-none hover:bg-glass px-6 py-3 rounded-full text-white font-semibold text-lg shadow-lg"
+                                    className="outline-none cursor-pointer hover:scale-105 transition-all duration-500 border-none hover:bg-glass px-6 py-3 rounded-full text-white text-center font-semibold text-lg shadow-lg"
                                     aria-label="Signin button"
                                     href="/signin"
                                 >
                                     Signin
                                 </Link>
                                 <Link
-                                    className="outline-none cursor-pointer hover:scale-105 transition-all duration-500 border-none bg-gradient-to-r from-emerald-400 to-teal-500 px-6 py-3 rounded-full text-white font-semibold text-lg shadow-lg"
+                                    className="outline-none text-center cursor-pointer hover:scale-105 transition-all duration-500 border-none bg-gradient-to-r from-emerald-400 to-teal-500 px-6 py-3 rounded-full text-white font-semibold text-lg shadow-lg"
                                     aria-label="Signup button"
                                     href="/register"
                                 >

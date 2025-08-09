8d87d4d75fcc7adb91f56a2bc164b3c4e6dc75fc
Mohammed Ehab <96921717+MOHAMED-EHAB-DEV@users.noreply.github.com>
2025-08-09 11:56:35 +0300

    Added canonical url to register page

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -4,26 +4,32 @@ import Link from "next/link";
 import {ArrowLeft} from "@/components/ui/svgs/Icons";
 
 const Page = () => {
+    const url = `${process.env.NEXT_PUBLIC_APP_URL}register`
     return (
-        <div className="min-h-screen h-full min-w-screen flex gap-4 flex-col items-center justify-center">
-            <Link
-                href="/"
-                className="inline-flex items-center -translate-x-30 text-gray-400 hover:text-white mb-8 transition-colors group"
-            >
-                <ArrowLeft className="w-4 h-4 mr-2 group-hover:-translate-x-1 transition-transform" />
-                Back to Home
-            </Link>
-            <div className="flex flex-col items-center p-6 justify-center bg-dark/70 border border-dark backdrop-blur-lg rounded-lg gap-4">
-                <div className="flex flex-col items-center justify-center gap-1">
-                    <Logo />
-                    <h1 className="text-white text-4xl font-bold">Register</h1>
+        <>
+            <Head>
+                <link rel="canonical" href={url} />
+            </Head>
+            <div className="min-h-screen h-full min-w-screen flex gap-4 flex-col items-center justify-center">
+                <Link
+                    href="/"
+                    className="inline-flex items-center -translate-x-30 text-gray-400 hover:text-white mb-8 transition-colors group"
+                >
+                    <ArrowLeft className="w-4 h-4 mr-2 group-hover:-translate-x-1 transition-transform" />
+                    Back to Home
+                </Link>
+                <div className="flex flex-col items-center p-6 justify-center bg-dark/70 border border-dark backdrop-blur-lg rounded-lg gap-4">
+                    <div className="flex flex-col items-center justify-center gap-1">
+                        <Logo />
+                        <h1 className="text-white text-4xl font-bold">Register</h1>
+                    </div>
+    
+                    <RegisterForm />
                 </div>
-
-                <RegisterForm />
+    
+                <div className="mt-4">Have an account? <Link href="/signin" className="text-gradient-purple">Signin Here</Link></div>
             </div>
-
-            <div className="mt-4">Have an account? <Link href="/signin" className="text-gradient-purple">Signin Here</Link></div>
-        </div>
+        </>
     )
 }
 export default Page

aa12a1975911889118aba8e6d24df3f46e9dab66
Mohammed Ehab <mohamedehabam99@hotmail.com>
2025-09-01 16:31:56 +0300

    fix: Update toggleFavorite to redirect to login without message for unauthorized users

--- context/UserContext.tsx
+++ context/UserContext.tsx
@@ -93,7 +93,7 @@ export function UserProvider({ children }: { children: ReactNode }) {
     };
 
     const toggleFavorite = (templateId: string) => {
-        if (!user) return router.push("/login?message=unauthorized");
+        if (!user) return router.push("/login");
         if (favoriteTemplates.includes(templateId)) removeFromFavorites(templateId);
         else addToFavorites(templateId);
     };

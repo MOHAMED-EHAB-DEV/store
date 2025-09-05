44e50ae3eeb2265241439f63ce9a25ca59e95560
Mohammed Ehab <mohamedehabam99@hotmail.com>
2025-09-05 13:57:25 +0300

    feat: wrap TemplateSkeleton in a div for consistent padding and layout

--- app/(main)/templates/[id]/loading.tsx
+++ app/(main)/templates/[id]/loading.tsx
@@ -2,6 +2,8 @@ import TemplateSkeleton from "@/components/ui/TemplateSkeleton";
 
 export default function Loading() {
   return (
-    <TemplateSkeleton />
+    <div className="pt-36 sm:pt-46 md:pt-36">
+      <TemplateSkeleton />
+    </div>
   );
 }

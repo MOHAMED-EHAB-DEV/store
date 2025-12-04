c9b37d8182fb6fe60335a34fe011a6050c4cb189
Mohammed Ehab <mohamed9919698@gmail.com>
2025-12-04 22:35:09 +0200

    feat: add smooth scrolling with Lenis and integrate it into a new root layout component with Navbar and Footer.

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { ReactLenis, useLenis } from 'lenis/react'
+import { ReactNode } from 'react';
 import Footer from "@/components/home/Footer";
 import Navbar from "@/components/home/Navbar";
 
@@ -6,17 +7,20 @@ export default function RootLayout(
     {
         children,
     }: Readonly<{
-        children: React.ReactNode;
+        children: ReactNode;
     }>) {
     return (
-        <div className="relative px-4 md:px-0 z-10 flex flex-col min-h-screen">
-            <Navbar/>
+        <>
+            <ReactLenis root />
+            <div className="relative px-4 md:px-0 z-10 flex flex-col min-h-screen">
+                <Navbar />
 
-            <div id="main-content" className="flex-1 flex flex-col items-center gap-3 justify-start">
-                {children}
+                <div id="main-content" className="flex-1 flex flex-col items-center gap-3 justify-start">
+                    {children}
+                </div>
+
+                <Footer />
             </div>
-
-            <Footer/>
-        </div>
+        </>
     )
 }

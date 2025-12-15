2804199f05cc6bd01c61c3f4bdd3ae4b2d90d74a
Mohammed Ehab <mohamedehabam99@hotmail.com>
2025-12-15 19:27:25 +0200

    commit

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useTheme } from "next-themes"
-import { Toaster as Sonner } from "sonner"
+import { Toaster as Sonner, toast as sonnerToast } from "sonner"
 
 type ToasterProps = React.ComponentProps<typeof Sonner>
 
@@ -28,4 +28,4 @@ const Toaster = ({ ...props }: ToasterProps) => {
   )
 }
 
-export { Toaster }
+export { Toaster, sonnerToast }

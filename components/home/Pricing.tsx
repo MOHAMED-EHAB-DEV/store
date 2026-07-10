952b62a0d9accf69caf047e7f53d0829be75dad8
Mohammed Ehab <mohamedehabam99@hotmail.com>
2026-07-10 16:46:55 +0300

    commit

--- components/home/Pricing.tsx
+++ components/home/Pricing.tsx
@@ -87,7 +87,7 @@ const Pricing = () => {
     <section className="w-full py-6 text-white">
       <div className="container mx-auto px-4 md:px-6">
         <div className="flex flex-col items-center justify-center space-y-4 text-center mb-12 pricing-header">
-          <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">Simple, transparent pricing</h2>
+          <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl font-paras">Simple, transparent pricing</h2>
           <p className="max-w-[900px] text-neutral-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
             Choose the plan that fits your needs. No hidden fees.
           </p>

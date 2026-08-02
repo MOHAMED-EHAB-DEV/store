8d17233563928f076d852ee42abc755fe13c7474
Mohammed Ehab <mohamedehabam99@hotmail.com>
2026-08-02 13:55:17 +0300

    commit

--- app/(app)/(admin)/loading.tsx
+++ app/(app)/(admin)/loading.tsx
@@ -2,10 +2,10 @@ import Loader from '@/components/ui/Loader';
 
 const Loading = () => {
   return (
-    <div className="flex items-center justify-center w-full h-full">
+    <div className="flex items-center justify-center w-full h-screen">
       <Loader />
     </div>
   )
 }
 
-export default Loading
+export default Loading;
